fd9b3ffa8ccae7cd27fc53ec1bae83cb9a344e9d
Hugh Earp <hugh.earp@gmail.com>
2023-08-05 18:29:44 +0200

    adding wrappers

--- config/wrappers.h
+++ config/wrappers.h
@@ -3,4 +3,13 @@
 #define SYM 3
 #define WMM 4
 
+#define MUTE &kp C_MUTE
+
 #define __THUMBS__ &mo NUM &kp SPACE &mo NAV &sl SYM &kp E &mo WMM
+
+#define __APTMAK_L1__ &kp W     &kp F     &kp P     &kp B
+#define __APTMAK_L2__ &kp MINUS &kp R &kp S &kp T &kp H &kp RET 
+#define __APTMAK_L3__ &key_repeat &kp C     &kp G     &kp D     &kp K
+#define __APTMAK_R1__ &kp J     &kp L     &kp U     &kp Y
+#define __APTMAK_R2__ &bspc_del &kp N &kp A &kp I &kp O &kp SQT
+#define __APTMAK_R3__ &kp V     &kp M     &kp COMMA &kp DOT   &kp FSLH
