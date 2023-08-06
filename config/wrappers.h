086478e4aeb50b6e2bbe99932b331c9b910a2fbb
Hugh Earp <hugh.earp@gmail.com>
2023-08-06 16:06:58 +0200

    adding num and sym layers

--- config/wrappers.h
+++ config/wrappers.h
@@ -20,10 +20,28 @@
 #define __APTMAK_R3__ &kp V     &kp M     &comma    &dot      &kp FSLH
 
 
-#define __NAV____R1__ &kp LG(Z) &kp LG(X) &kp LG(C) &kp LG(V)  &kp LG(Y)
-#define __NAV____R2__ __CALLUM_R__                             &kp RET
-#define __NAV____R3__ &none     &none     &kp ESC   &kp TAB    &none
+#define __NAV____L1__ &kp LG(Z) &kp LG(X) &kp LG(C) &kp LG(V)  &kp LG(Y)
+#define __NAV____L2__ __CALLUM_L__                             &kp RET
+#define __NAV____L3__ &none     &none     &kp ESC   &kp TAB    &none   // Need to add the swapper
 
-#define __NAV____L1__ &none     &none     &none     &none     &none
-#define __NAV____L2__ &kp BSPC  &kp LEFT  &kp DOWN  &kp UP    &kp RIGHT
-#define __NAV____L3__ &none     &none     &none     &none     &none
+#define __NAV____R1__ &none     &none     &none     &none     &none    // Add back the LA(arrows)?
+#define __NAV____R2__ &kp BSPC  &kp LEFT  &kp DOWN  &kp UP    &kp RIGHT
+#define __NAV____R3__ &none     &none     &none     &none     &none    // Add back the LG(arrows)?
+
+
+#define __NUM____L1__ &kp E      &none     &none     &none     &none   // Need to think about what goes here
+#define __NUM____L2__ __CALLUM_L__                             &kp RET
+#define __NUM____L3__ &kp LS(LG(TAB) &kp LG(TAB) &kp ESC &kp TAB &none // Need to think about what goes on inner index key
+
+#define __NUM____R1__ &kp MINUS  &kp N7    &kp N8    &kp N9    &kp EQUAL
+#define __NUM____R2__ &bspc_del  &kp N4    &kp N5    &kp N6    &kp N0
+#define __NUM____R3__ &dot       &kp N1    &kp N2    &kp N3    &kp PLUS
+
+
+#define __SYM____L1__ &kp LT     &kp LBRC  &kp LPAR  &kp LBKT  &none   // Need to think about what goes on inner index key
+#define __SYM____L2__ &kp POUND  &kp ASTRK &kp EXCL  &kp QMARK &none   // Need to think about what goes on inner index key
+#define __SYM____L3__ &kp PRCNT  &kp AT    &kp DLLR  &kp AMPS  &kp GRAVE
+
+#define __SYM____R1__ &none      &kp RBKT  &kp RPAR  &kp RBRC  &kp RT  // Need to think about what goes on inner index key
+#define __SYM____R2__ &none      __CALLUM_R__                          // Need to add macro for word delete
+#define __SYM____R3__ &kp TILDE  &kp CARET &kp BSLH  &none     &none   // Need to add macros for NRC and M&E
